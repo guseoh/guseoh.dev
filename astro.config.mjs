bd89d03f40ccbe998ff6a1b87d7f3bea4641f038
guseoh <guseoh@naver.com>
2026-05-20 17:10:17 +0900

    Fix markdown alerts, TOC headings, and sidebar layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 ﻿import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import { remarkAlert } from "remark-github-blockquote-alert";
 
 export default defineConfig({
   site: "https://guseoh.github.io",
   markdown: {
+    remarkPlugins: [remarkAlert],
     syntaxHighlight: {
       type: "shiki",
       excludeLangs: ["math"]
